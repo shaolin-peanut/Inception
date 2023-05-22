2f93211aa87cae17f18455756acaf2f11b4bbcc7
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-22 16:50:36 +0200

    incorrect wp-config variables, fixed

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
-define('DB_USER', getenv('WORDPRESS_DB_USER'));
-define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
-define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
+define('DB_NAME', getenv('WP_DB_NAME'));
+define('DB_USER', getenv('WP_DB_USER'));
+define('DB_PASSWORD', getenv('WP_DB_PASSWORD'));
+define('DB_HOST', getenv('WP_DB_HOST'));
 // define('DB_CHARSET', 'utf8');
 // define('DB_COLLATE', '');
 
