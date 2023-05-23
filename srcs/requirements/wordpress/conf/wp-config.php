a7db02e5152dce48a6e26f8a612259629c65bb09
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-23 14:06:20 +0200

    changed copy of wp-config to /var/www/wordpress isntead of /usr/share/wordpress

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -12,18 +12,6 @@ define('DB_PASSWORD', getenv('WP_DB_PASSWORD'));
 echo 'WP_DB_HOST: ' . getenv('WP_DB_HOST') . "\n";
 define('DB_HOST', getenv('WP_DB_HOST') . ':3306');
 
-// define('DB_CHARSET', 'utf8');
-// define('DB_COLLATE', '');
-
-// define('AUTH_KEY',         getenv('AUTH_KEY'));
-// define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
-// define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
-// define('NONCE_KEY',        getenv('NONCE_KEY'));
-// define('AUTH_SALT',        getenv('AUTH_SALT'));
-// define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
-// define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
-// define('NONCE_SALT',       getenv('NONCE_SALT'));
-
 define('WP_DEBUG', true);
 
 $table_prefix  = 'wp_';
