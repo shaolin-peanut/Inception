f84c88cb1f22a45f1b6243ecb3d46786373d34e4
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-28 14:07:31 +0200

    fixing mariadb location for data file, conf, etc

--- srcs/requirements/mariadb/tools/kickstart.sh
+++ srcs/requirements/mariadb/tools/kickstart.sh
@@ -36,7 +36,12 @@ mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;";
 mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
 
 # start MariaDB again, in the background
-mysqld_safe --datadir=/usr --user=mysql &
+mysqld_safe --datadir=/var/lib/mysql --user=mysql &
+
+# Wait for MariaDB to start
+until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping >/dev/null 2>&1; do
+    sleep 1
+done
 
 # restart (so that root pswd is integrated)
 # exec /usr/bin/mysqld_safe
