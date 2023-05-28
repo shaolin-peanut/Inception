6eabec56c39af997a3ac4f781cb21ca39f0a25a4
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-28 13:08:09 +0200

    changed wordpress install location

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p /var/www/html
-cd /var/www/html
+mkdir -p /var/www/wordpress
+cd /var/www/wordpress
 rm -rf *
 
 until nc -z mariadb 3306; do
@@ -18,15 +18,12 @@ mv wp-cli.phar /usr/local/bin/wp
 
 # https://developer.wordpress.org/cli/commands/core/
 wp core download --allow-root
-rm /var/www/html/wp-config-sample.php
+rm /var/www/wordpress/wp-config-sample.php
 wp config create \
     --dbname=$MYSQL_DATABASE \
     --dbuser=$MYSQL_USER \
     --dbpass=$MYSQL_PASSWORD \
-    --dbhost=mariadb \
-    --extra-php <<EOF
-    define('WP_REDIS_HOST', 'redis');
-    define('WP_REDIS_PORT', 6379); 
+    --dbhost=mariadb
 EOF
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
