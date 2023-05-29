96503f1cc164fd576b3e24c00e0a338d39dc0b53
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-29 16:46:27 +0200

    added  bind_address 0000 when restarting mysqld_safe

--- srcs/requirements/mariadb/tools/kickstart.sh
+++ srcs/requirements/mariadb/tools/kickstart.sh
@@ -36,7 +36,7 @@ mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;";
 mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
 
 # start MariaDB again, in the background
-mysqld_safe --datadir=/var/lib/mysql --user=mysql &
+mysqld_safe --datadir=/var/lib/mysql --user=mysql --bind-address=0.0.0.0 &
 
 # restart (so that root pswd is integrated)
 # exec /usr/bin/mysqld_safe
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -10,7 +10,7 @@ done
 if [ ! -f "wp-config.php" ]; then
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-chmod +x wp-cli.phar 
+chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
