b9a5485da8901e7c0832fd3ee6707b94a60b4e0c
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-31 13:59:39 +0200

    db users fixed in wp and mariadb scripts

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,12 +1,19 @@
 #!bin/bash
+set -x
+
 sleep 10
 cat /var/www/wordpress/wp-config.php
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
-    					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+    wp config create --allow-root \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$WP_DB_USER \
+        --dbpass=$WP_DB_PASSWORD \
+        --dbhost=mariadb:3306 \
+        --path='/var/www/wordpress'
+
 
 sleep 2
-wp core install     --url=$DOMAIN_NAME --title="WOAH" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+wp core install     --url=$DOMAIN_NAME --title="WOAH" --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
 wp user create      --allow-root --role=author $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PWD --path='/var/www/wordpress' >> /log.txt
 fi
 
