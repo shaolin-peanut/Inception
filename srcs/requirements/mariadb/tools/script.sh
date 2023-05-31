6333679edfbfa011fe5a64cb771d9c74c01af46b
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-31 18:26:33 +0200

    started mysql before trying to check if there's a user in it

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,18 +4,19 @@ set -x
 echo "mariadb-server mysql-server/root_password password root123" | debconf-set-selections
 echo "mariadb-server mysql-server/root_password_again password root123" | debconf-set-selections
 
-if mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$MYSQL_USER' LIMIT 1);" | grep -q '1'; then
-    mysql_install_db --user=mysql --ldata=/var/lib/mysql
-
 chown -R mysql:mysql /var/lib/mysql
 chmod 644 /var/lib/mysql
 
 # start mariadb
 mysqld_safe --datadir=/var/lib/mysql --user=mysql --bind-address=0.0.0.0 &
 
+if mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$MYSQL_USER' LIMIT 1);" | grep -q '1'; then
+
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
+
 # wait for mariadb to start
 until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping >/dev/null 2>&1; do
-	sleep 1
+    sleep 1
 done
 
 mysql -uroot -p${MYSQL_ROOT_PASSWORD} <<- EOF
@@ -28,7 +29,7 @@ mysql -uroot -p${MYSQL_ROOT_PASSWORD} <<- EOF
     FLUSH PRIVILEGES;
 EOF
 
-# kill process
+    # kill process
 mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
 
 fi
