6d68647323bf548fe6bee551b17cb8ea4f5a1893
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-31 18:32:03 +0200

    breaking out the loop

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -x
 
-echo "mariadb-server mysql-server/root_password password root123" | debconf-set-selections
-echo "mariadb-server mysql-server/root_password_again password root123" | debconf-set-selections
-
 chown -R mysql:mysql /var/lib/mysql
 chmod 644 /var/lib/mysql
 
 # start mariadb
 mysqld_safe --datadir=/var/lib/mysql --user=mysql &
 
-if mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$MYSQL_USER' LIMIT 1);" | grep -q '1'; then
-
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql
+fi
 
 # wait for mariadb to start
 until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping >/dev/null 2>&1; do
@@ -32,10 +29,8 @@ EOF
     # kill process
 mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
 
-fi
-
 # start MariaDB again, in the background
-mysqld_safe --datadir=/var/lib/mysql --user=mysql &
+mysqld_safe --datadir=/var/lib/mysql --user=mysql
 
 # restart (so that root pswd is integrated)
 # exec /usr/bin/mysqld_safe
