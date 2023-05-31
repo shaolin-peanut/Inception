b6228a9524fa280b9200fb76589e6865747d4ede
shaolin-peanut <shaolinpeanut@gmail.com>
2023-05-31 16:11:23 +0200

    new mariadb script

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -6,8 +6,8 @@ cat /var/www/wordpress/wp-config.php
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
     wp config create --allow-root \
         --dbname=$MYSQL_DATABASE \
-        --dbuser=$WP_DB_USER \
-        --dbpass=$WP_DB_PASSWORD \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
         --dbhost=mariadb:3306 \
         --path='/var/www/wordpress'
 
@@ -16,6 +16,13 @@ sleep 2
 wp core install     --url=$DOMAIN_NAME --title="WOAH" --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
 wp user create      --allow-root --role=author $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PWD --path='/var/www/wordpress' >> /log.txt
 fi
+	
+
+# if /run/php folder does not exist, create it
+if [ ! -d /run/php ]; then
+    mkdir ./run/php
+fi
+/usr/sbin/php-fpm7.3 -F
 
 # echo "define( 'CONCATENATE_SCRIPTS', false );" >> /var/www/wordpress/wp-config.php
 # echo "define( 'SCRIPT_DEBUG', true );" >> /var/www/wordpress/wp-config.php
@@ -26,11 +33,3 @@ fi
 # echo "define( 'WP_DEBUG_LOG', true);" >> /var/www/wordpress/wp-config.php
 # echo "define( 'WP_DEBUG_DISPLAY', false);" >> /var/www/wordpress/wp-config.php
 # echo "define('WP_ALLOW_REPAIR', true);" >> /var/www/wordpress/wp-config.php
-
-	
-
-# if /run/php folder does not exist, create it
-if [ ! -d /run/php ]; then
-    mkdir ./run/php
-fi
-/usr/sbin/php-fpm7.3 -F
