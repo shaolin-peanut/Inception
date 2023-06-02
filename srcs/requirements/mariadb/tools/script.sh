23ab3727cf2d81450e034bbc5be2f773728d59bd
shaolin-peanut <shaolinpeanut@gmail.com>
2023-06-02 15:59:49 +0200

    mmh

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,7 +4,7 @@ set -x
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --ldata=/var/lib/mysql
 fi
-chmod 644 /var/lib/mysql
+# chmod 644 /var/lib/mysql
 
 if [ -f /var/www/wordpress/wp-config.php ]; then
 
