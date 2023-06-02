ca7c133c6a457fc166d8be2531f0211b0611cbaf
shaolin-peanut <shaolinpeanut@gmail.com>
2023-06-02 17:32:55 +0200

    wp config as www-data

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,28 +1,41 @@
-#!bin/bash
+#!/bin/bash
 set -x
 
 sleep 10
 # cat /var/www/wordpress/wp-config.php
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp config create --allow-root \
+    su -s /bin/bash -c "wp config create \
         --dbname=$MYSQL_DATABASE \
         --dbuser=$MYSQL_USER \
         --dbpass=$MYSQL_PASSWORD \
         --dbhost=mariadb:3306 \
-        --path='/var/www/wordpress'
-
+        --path='/var/www/wordpress'" www-data
 
     sleep 2
-    wp core install     --url=$DOMAIN_NAME --title="WOAH" --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-    wp user create      --allow-root --role=author $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PWD --path='/var/www/wordpress' >> /log.txt
+    su -s /bin/bash -c "wp core install \
+        --url=$DOMAIN_NAME \
+        --title=\"WOAH\" \
+        --admin_user=$MYSQL_USER \
+        --admin_password=$MYSQL_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --allow-root \
+        --path='/var/www/wordpress'" www-data
+
+    su -s /bin/bash -c "wp user create \
+        --allow-root \
+        --role=author \
+        $WP_USER \
+        $WP_USER_EMAIL \
+        --user_pass=$WP_USER_PWD \
+        --path='/var/www/wordpress' >> /log.txt" www-data
 fi
 	
-
 # if /run/php folder does not exist, create it
 if [ ! -d /run/php ]; then
     mkdir ./run/php
 fi
 /usr/sbin/php-fpm7.3 -F
+
 
 # echo "define( 'CONCATENATE_SCRIPTS', false );" >> /var/www/wordpress/wp-config.php
 # echo "define( 'SCRIPT_DEBUG', true );" >> /var/www/wordpress/wp-config.php
