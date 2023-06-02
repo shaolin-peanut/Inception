3d3e48e9015c402614a374ef238984c48e50e46d
shaolin-peanut <shaolinpeanut@gmail.com>
2023-06-02 16:10:44 +0200

    experimeting with mysql user instead of root lol

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -12,11 +12,14 @@ if [ -f /var/www/wordpress/wp-config.php ]; then
 mysqld_safe --datadir=/var/lib/mysql --user=mysql --bind-address=0.0.0.0 &
 
 # wait for mariadb to start
-until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping >/dev/null 2>&1; do
+# until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping >/dev/null 2>&1; do
+#     sleep 1
+# done
+until mysqladmin -u mysql ping >/dev/null 2>&1; do
     sleep 1
 done
 
-mysqladmin -u root password '${MYSQL_ROOT_PASSWORD}'
+# mysqladmin -u root password '${MYSQL_ROOT_PASSWORD}'
     # SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
     # GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;
     # DELETE FROM mysql.user WHERE user != 'root' AND user != 'mariadb.sys' OR (user = 'root' AND host != 'localhost');
@@ -25,7 +28,8 @@ mysqladmin -u root password '${MYSQL_ROOT_PASSWORD}'
     # GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@'%';
     # FLUSH PRIVILEGES;
 
-mysql -u root -p${MYSQL_ROOT_PASSWORD} <<- EOF
+# mysql -u root -p${MYSQL_ROOT_PASSWORD} <<- EOF
+mysql -u mysql <<- EOF
     CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
     CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON ${MSYQL_DATABASE}.* TO ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
